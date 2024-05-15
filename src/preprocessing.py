3959f6a985d91025e53546e0c3693e0bc21c07aa
Anna Senent <anna.senent.julian@avanade.com>
2024-05-15 12:02:37 +0200

    preprocessing adapted to use the new centralized pipeline

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -3,10 +3,11 @@ from typing import Callable
 from pathlib import WindowsPath
 import pandas as pd
 from preprocessing_functions import clean_text, add_polarity_label
-
+from centralized_preprocessing import CentralizedPreprocessing
 
 def processing_pipeline(
-    path_dataset: WindowsPath, modify_func: Callable[[pd.DataFrame], pd.DataFrame]
+    path_dataset: WindowsPath, modify_func: Callable[[pd.DataFrame], pd.DataFrame],
+    pipeline: CentralizedPreprocessing
 ) -> pd.DataFrame:
 
     df = pd.read_csv(path_dataset, encoding="utf8")
@@ -17,8 +18,8 @@ def processing_pipeline(
     # add extra polarity label
     df = modify_func(df)
 
-    df["text_cleaned"] = df.Text.apply(lambda x: clean_text(x))
-    df["summary_cleaned"] = df.Summary.apply(lambda x: clean_text(x))
+    df["text_cleaned"] = df.Text.apply(lambda x: pipeline.preprocess(x))
+    df["summary_cleaned"] = df.Summary.apply(lambda x: pipeline.preprocess(x))
 
     return df
 
@@ -29,7 +30,9 @@ def main() -> pd.DataFrame:
     parser.add_argument("--data", type=str, help="path to cloud input data")
     parser.add_argument("--output", type=str, help="path to cloud output data")
     args = parser.parse_args()
-    df = processing_pipeline(path_dataset=args.data, modify_func=add_polarity_label)
+
+    pipeline = CentralizedPreprocessing()
+    df = processing_pipeline(path_dataset=args.data, modify_func=add_polarity_label, pipeline=pipeline)
 
     df.to_csv(args.output, index=False, encoding="utf-8")
 
